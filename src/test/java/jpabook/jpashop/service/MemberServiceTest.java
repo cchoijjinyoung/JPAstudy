21dc2964eec1504d02f9626ad70c22842ad022d3
cchoijjinyoung <cgy7127@naver.com>
2022-06-14 22:44:01 +0900

    Item service

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,25 +1,51 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class) // junit 실행할 때 스프링이랑 같이 실행할래.
+@SpringBootTest // 스프링부트를 띄운상태로 테스트 하기 위해서는 필요하다.
+@Transactional // 테스트가 끝나면 rollback() 되도록 설정이 되어있다.
 public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
     @Test
     public void 회원가입() throws Exception {
         //given
-
+        Member member = new Member();
+        member.setName("kim");
         //when
-
+        Long saveId = memberService.join(member);
         //then
+        assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생해야 한다.
 
         //then
+        fail("예외가 발생해야 한다.");
     }
 }
